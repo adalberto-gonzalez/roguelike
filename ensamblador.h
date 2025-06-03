424b615b6c889a4f3f64de91069c1bee86d438c0
Adalberto Gonzalez <adalberto.gonzalez.gil@uabc.edu.mx>
2025-06-03 16:42:41 -0700

    Update ensamblador.h

--- ensamblador.h
+++ ensamblador.h
@@ -1,30 +1,30 @@
 #include "raylib.h"
 
-Vector2 VectorAdd();
-Vector2 VectorSub();
-Vector2 VectorNorm();
-Vector2 VectorMul();
+Vector2 VectorAdd(Vector2 a, Vector2 b);
+Vector2 VectorSub(Vector2 a, Vector2 b);
+Vector2 VectorNorm(Vector2 a);
+Vector2 VectorMul(Vector2 a, Vector2 b);
 Vector2 PlayerMovement();
 
-Vector2 VectorSum(){
+Vector2 VectorSum(Vector2 a, Vector2 b){
     __asm__(".intel_syntax noprefix;"
             
 
             ".att_syntax;");
 }
-Vector2 VectorSub(){
+Vector2 VectorSub(Vector2 a, Vector2 b){
     __asm__(".intel_syntax noprefix;"
             
 
             ".att_syntax;");
 }
-Vector2 VectorNorm(){
+Vector2 VectorNorm(Vector2 a){
     __asm__(".intel_syntax noprefix;"
             
 
             ".att_syntax;");
 }
-Vector2 VectorMul(){
+Vector2 VectorMul(Vector2 a, Vector2 b){
     __asm__(".intel_syntax noprefix;"
             
 
